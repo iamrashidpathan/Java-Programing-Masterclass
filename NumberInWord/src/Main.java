e038da575ee2a1440716b302763eba2c52a6f7a4
Rashid Reyaz Khan <khanrashid11a@gmail.com>
2020-06-15 09:27:13 +0530

    Coding Exercise
    Prints numbers in words from 0 to 9 and prints OTHER for any number out of this range. Switch case was used

---
+++ NumberInWord/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(1);
+        NumberInWord.printNumberInWord(2);
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(54);
+        NumberInWord.printNumberInWord(-23);
+    }
+}
