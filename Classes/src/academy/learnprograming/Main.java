de15b6d6c4fbb5bde586e6a29e7b37030690a2ea
Rashid Reyaz Khan <khanrashid11a@gmail.com>
2020-06-22 12:09:18 +0530

    Classes tutorial codes.
    codes written for classes tutorial

---
+++ Classes/src/academy/learnprograming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprograming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("Carrera");
+        System.out.println("Model is "+porsche.getModel());
+    }
+}
