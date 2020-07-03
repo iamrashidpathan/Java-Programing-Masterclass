6b1087a94e9b245609302d591720b1e2bbafd0e7
Rashid Reyaz Khan <khanrashid11a@gmail.com>
2020-07-03 12:21:59 +0530

    Arrays
    Codes written for array tutorial videos.

--- Arrays/src/academy/learnprograming/Main.java
+++ Arrays/src/academy/learnprograming/Main.java
@@ -3,16 +3,25 @@ package academy.learnprograming;
 import java.util.Scanner;
 
 public class Main {
-
+    
+    // creating an scanner object
     private static Scanner scanner = new Scanner(System.in);
+    
+    // main class
     public static void main(String[] args) {
+        //callling getIntegers with 5 to get a array of 5 elements.
         int[] myIntegers = getIntegers(5);
+        
+        // printing out all the elements of array,
         for (int i = 0; i<myIntegers.length; i++) {
             System.out.println("Element "+i+" was "+myIntegers[i]);
         }
+        
+        // calling getAverage method and printing the average of the elements of array.
         System.out.println("Average of entered numbers is "+getAverage(myIntegers));
     }
-
+    
+    // getInteregs method takes intrger inputs from user and stores it in an array then returns the array.
     public static int[] getIntegers(int number) {
         System.out.println("Enter "+number+" Integer values.");
         int[] array = new int[number];
@@ -23,6 +32,7 @@ public class Main {
         return array;
     }
 
+    //returns the average of the elements of the integer array
     public static double getAverage(int[] array) {
         int sum = 0;
         for (int i = 0; i<array.length; i++) {
